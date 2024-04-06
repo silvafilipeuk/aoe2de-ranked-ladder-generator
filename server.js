261afdb02b1d400854e2c19fe03f6a4fa11fe704
Filipe da Silva <silvafilipeuk@gmail.com>
2024-04-06 21:57:12 +0100

    Changed server port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
-const PORT = 9090;
+const PORT = 80;
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+	console.log(`Server listening on port ${PORT}`);
 });
