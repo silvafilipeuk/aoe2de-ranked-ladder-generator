ca489b0d4676a3abc12f94499afa3e72c35709aa
Filipe <silvafilipeuk@gmail.com>
2024-04-09 22:59:25 +0100

    Implementing https functionality

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const app = require("./app");
+const https = require("https")
+const fs = require("fs")
+
 const PORT = 80;
+const PORTHTTPS = 443;
 
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`);
 });
+
+https.createServer(app).listen(PORTHTTPS, () => {
+	console.log("https server listening on port 443");
+})
