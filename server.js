6d595c38e3786a58f335359bfa4bce6c693e14e3
Filipe <silvafilipeuk@gmail.com>
2024-04-09 23:05:54 +0100

    Implementing https functionality

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`);
 });
 
-https.createServer(app).listen(PORTHTTPS, () => {
+https
+	.createServer(
+	// Provide the private and public key to the server by reading each
+	// file's content with the readFileSync() method.
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+).listen(PORTHTTPS, () => {
 	console.log("https server listening on port 443");
 })
