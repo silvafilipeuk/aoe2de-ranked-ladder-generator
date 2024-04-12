7fb28602066351487f82c68717ddecba5856b8ee
Filipe da Silva <silvafilipeuk@gmail.com>
2024-04-12 13:13:38 +0100

    commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ https
 		// Provide the private and public key to the server by reading each
 		// file's content with the readFileSync() method.
 		{
-			key: fs.readFileSync("filipedasilva_co_uk.pem"),
+			key: fs.readFileSync("private_key.key"),
 			cert: fs.readFileSync("filipedasilva_co_uk.pem"),
 		},
 		app
